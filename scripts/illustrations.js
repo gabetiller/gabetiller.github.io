a6b432f65959a3588733e06618df1be9cac096dc
Gabe Tiller <gabectiller@gmail.com>
2018-02-05 17:04:35 -0500

    illustration script

--- scripts/illustrations.js
+++ scripts/illustrations.js
@@ -1,1 +1,36 @@
+var canvas;
+var r, g, b;
+var stroke_weight
 
+function windowResized() {
+  resizeCanvas(windowWidth, window.innerHeight)
+}
+function setup() {
+  canvas = createCanvas(windowWidth, window.innerHeight);
+
+  canvas.position(0,0);
+  canvas.style('z-index', '-1');
+  background(255);
+  r = random(255)
+  g = random(255)
+  b = random(255)
+
+};
+
+
+function draw() {
+  if (mouseIsPressed) {
+    line(pmouseX, pmouseY, mouseX, mouseY);
+    r = random(255)
+    g = random(255)
+    b = random(255)
+    strokeWeight(20);
+    stroke(r, g, b);
+  }
+};
+
+$('body').awesomeCursor('paint-brush', {
+  color: 'red',
+  size: 30,
+  hotspot: [5, 25]
+});
